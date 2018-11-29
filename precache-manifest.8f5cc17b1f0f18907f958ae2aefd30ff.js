30e2513beefcbb90a3b567e40ba67623bfec393c
daaef <realafe21@gmail.com>
2018-11-29 23:20:32 +0100

    reactified version, hope works

---
+++ precache-manifest.8f5cc17b1f0f18907f958ae2aefd30ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9988f50b77f056a25b4ed8a3e80a14bb",
+    "url": "/static/media/request.9988f50b.png"
+  },
+  {
+    "revision": "a2027a37331cb3d24b15",
+    "url": "/static/css/main.e838c0bd.chunk.css"
+  },
+  {
+    "revision": "d54f5511639445c2ec34",
+    "url": "/static/js/1.d54f5511.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "887debd0d0e4f9405d6bc60ad4699d82",
+    "url": "/static/media/heroLogo.887debd0.png"
+  },
+  {
+    "revision": "bbe2cddfd106089c8ee9e8163b17974d",
+    "url": "/static/media/handPhone.bbe2cddf.png"
+  },
+  {
+    "revision": "a2027a37331cb3d24b15",
+    "url": "/static/js/main.a2027a37.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5eed8215d73bbdf1fa2c1b298f94b89c",
+    "url": "/static/media/phoneGuy.5eed8215.png"
+  },
+  {
+    "revision": "8b8cb45017d50aaca16069fa173a2ebb",
+    "url": "/static/media/bgLanding.8b8cb450.png"
+  },
+  {
+    "revision": "d933a9696c7114f8f3f28261ee986480",
+    "url": "/static/media/bg2nd.d933a969.png"
+  },
+  {
+    "revision": "aac70af8ae381f6923aa9d5ae992dcb0",
+    "url": "/static/media/commuter-BG.aac70af8.png"
+  },
+  {
+    "revision": "90c9e8b82360bcced0a6378fa50caa14",
+    "url": "/index.html"
+  }
+];
